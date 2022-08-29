da5fe061e80ef640491810538aa36d5307313f26
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-29 20:18:21 +1000

    Added slash command group raw_num_gen

--- coolbot.py
+++ coolbot.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from pydoc import describe
 import random
 import textwrap
 from typing import Tuple
@@ -13,8 +14,10 @@ _secrets = dotenv_values(".env", verbose=True)
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = bridge.Bot(
-    command_prefix="$", intents=intents)
+bot = commands.Bot(
+    command_prefix="$!", intents=intents)
+default_decorator = commands.command
+raw_gen_decorator = bot.create_group("raw_number_gen", "Use these functions to generate random numbers",).command
 
 _previousNums = []
 _defaultLowNum = 0
@@ -43,21 +46,21 @@ async def _int(ctx, x: str) -> int:
   except ValueError:
     await ctx.send(f"ERROR: **{x}** is not a valid number!")
 
-@bot.bridge_command()
+@default_decorator()
 async def test(ctx):
     await ctx.respond(f"__test__")
 
-@bot.bridge_command()
+@raw_gen_decorator(description="Generate a random number between the set default low (0) and the set default high (6)")
 async def gen_random_num(ctx):
     await ctx.respond(f"random num between 0 and 6: \n{_gen_num()[0]}")
 
-@bot.bridge_command()
+@raw_gen_decorator(description="Generate a random number between the set default low (0) and the specified number")
 async def gen_random_num_to(ctx, num):
     num: int = await _int(ctx, num)
     _num = _gen_num(..., num)
     await ctx.respond(f"{_num[1]}{_gen_prefix(_num[1])} random num is: \n**{_num[0]}**")
     
-@bot.bridge_command()
+@raw_gen_decorator(description="Generate the specified amount of random numbers")
 async def gen_n_random_nums(ctx, num):
     num: int = await _int(ctx, num)
     assert num > 0 # Errors otherwise
@@ -65,30 +68,30 @@ async def gen_n_random_nums(ctx, num):
     nums_str = textwrap.fill(", ".join(map(lambda n: str(n[0]), _nums)), 30)
     _beginning_prefix = f"{_nums[0][1]}{_gen_prefix(_nums[0][1])}"
     _ending_prefix = f"{_nums[-1][1]}{_gen_prefix(_nums[-1][1])}"
-    await ctx.respond(f"{_beginning_prefix} to {_ending_prefix} random numbers: \n{nums_str}")
+    await ctx.respond(f"Generating {num} random numbers, which are the {_beginning_prefix} to {_ending_prefix} numbers generated (see /history): \n{nums_str}")
 
-@bot.bridge_command()
+@raw_gen_decorator()
 async def set_default_low_num(ctx, num):
     num: int = await _int(ctx, num)
     global _defaultLowNum
     _defaultLowNum = num
     await ctx.respond(f"Default low number is now: {_defaultLowNum}\nNow, random numbers will be within the range of [{_defaultLowNum}, {_defaultHighNum}]")
 
-@bot.bridge_command()
+@raw_gen_decorator()
 async def set_default_high_num(ctx, num):
     num: int = await _int(ctx, num)
     global _defaultHighNum
     _defaultHighNum = num
     await ctx.respond(f"Default high number is now: {_defaultHighNum}\nNow, random numbers will be within the range of [{_defaultLowNum}, {_defaultHighNum}]")
 
-@bot.bridge_command()
+@raw_gen_decorator()
 async def reset_defaults(ctx):
     global _defaultLowNum, _defaultHighNum
     _defaultLowNum = 0
     _defaultHighNum = 6
     await ctx.respond(f"Defaults have been reset to: [{_defaultLowNum}, {_defaultHighNum}]")
 
-@bot.bridge_command()
+@raw_gen_decorator()
 async def history(ctx):
     _string = ""
     for i, num in enumerate(_previousNums):
@@ -101,7 +104,7 @@ async def history(ctx):
     else:
         await ctx.respond(f"History is empty! May have /clear ed history")
 
-@bot.bridge_command()
+@raw_gen_decorator()
 async def clear_history(ctx):
     _previousNums.clear()
     await ctx.respond("History cleared :smile:")
