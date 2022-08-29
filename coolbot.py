942cfd64c6d686b5b57baf812f0a98f3db3e779a
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-29 19:44:49 +1000

    Slash commands now respond so no error messages

--- coolbot.py
+++ coolbot.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
-import math
 import random
+import textwrap
 from typing import Tuple
 import discord
 from discord.ext import bridge
@@ -50,22 +50,25 @@ async def test(ctx):
 @bot.bridge_command()
 async def gen_random_num(ctx):
     await ctx.send(f"random num between 0 and 6: \n{_gen_num()[0]}")
+    await ctx.respond(f"Done")
 
 @bot.bridge_command()
 async def gen_random_num_to(ctx, num):
     num: int = await _int(num)
     _num = _gen_num(..., num)
     await ctx.send(f"{_num[1]}{_gen_prefix(_num[1])} random num is: \n**{_num[0]}**")
+    await ctx.respond(f"Done")
     
 @bot.bridge_command()
 async def gen_n_random_nums(ctx, num):
     num: int = await _int(ctx, num)
     assert num > 0 # Errors otherwise
     _nums = [_gen_num(..., ...) for i in range(num)]
-    nums_str = "\t".join(map(lambda n: str(n[0]), _nums))
+    nums_str = textwrap.fill(", ".join(map(lambda n: str(n[0]), _nums)))
     _beginning_prefix = f"{_nums[0][1]}{_gen_prefix(_nums[0][1])}"
     _ending_prefix = f"{_nums[-1][1]}{_gen_prefix(_nums[-1][1])}"
     await ctx.send(f"{_beginning_prefix} to {_ending_prefix} random numbers: \n{nums_str}")
+    await ctx.respond(f"Done")
 
 @bot.bridge_command()
 async def set_default_low_num(ctx, num):
@@ -73,6 +76,7 @@ async def set_default_low_num(ctx, num):
     global _defaultLowNum
     _defaultLowNum = num
     await ctx.send(f"Default low number is now: {_defaultLowNum}\nNow, random numbers will be within the range of [{_defaultLowNum}, {_defaultHighNum}]")
+    await ctx.respond(f"Done")
 
 @bot.bridge_command()
 async def set_default_high_num(ctx, num):
@@ -80,21 +84,26 @@ async def set_default_high_num(ctx, num):
     global _defaultHighNum
     _defaultHighNum = num
     await ctx.send(f"Default high number is now: {_defaultHighNum}\nNow, random numbers will be within the range of [{_defaultLowNum}, {_defaultHighNum}]")
+    await ctx.respond(f"Done")
 
 @bot.bridge_command()
 async def history(ctx):
     _string = ""
     for i, num in enumerate(_previousNums):
-      _string += f"{i}{_gen_prefix(i)}: {num}\n"
+      # Indexes start at 0, but we want to start at 1 for '1st', '2nd', etc. not '0th', '1st', etc.
+      i += 1 
+      _string += textwrap.fill(f"**{i}**{_gen_prefix(i)}: {num},\t", width=60)
     if _string != "":
       await ctx.send(f"History random numbers: \n{_string}")
     else:
         await ctx.send(f"History is empty! May have /clear ed history")
+    await ctx.respond(f"Done")
 
 @bot.bridge_command()
 async def clear_history(ctx):
     _previousNums.clear()
     await ctx.send("History cleared :smile:")
+    await ctx.respond(f"Done")
 
 try:
   bot.run(_secrets["DISCORD_BOT_TOKEN"])
@@ -102,3 +111,4 @@ except Exception as e:
   print("ERROR, if shard permissions error please give the bot permissions!")
   
   raise e
+print('hello world')
